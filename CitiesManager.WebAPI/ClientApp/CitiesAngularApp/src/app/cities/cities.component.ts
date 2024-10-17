486f5e94446365a562370590c54cd83162418204
danylo kozlovskij <danilokozlovskij@gmail.com>
2024-10-17 12:48:02 +0300

    added html formGroup

--- CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
+++ CitiesManager.WebAPI/ClientApp/CitiesAngularApp/src/app/cities/cities.component.ts
@@ -2,20 +2,25 @@ import { Component } from '@angular/core';
 import { City } from '../models/city';
 import { CitiesService } from '../services/cities.service';
 import { CommonModule } from '@angular/common';
+import { FormControl, FormGroup, ReactiveFormsModule, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-cities',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, ReactiveFormsModule],
   templateUrl: './cities.component.html',
   styleUrl: './cities.component.css'
 })
 export class CitiesComponent {
   cities: City[] = [];
+  postCityForm: FormGroup;
   constructor(private citiesService: CitiesService) {
-
+    this.postCityForm = new FormGroup({
+      name: new FormControl(null, [Validators.required])
+    })
   }
-  ngOnInit() {
+
+  loadCities() {
     this.citiesService.getCities().subscribe({
        next: (response: City[]) => {
         this.cities = response;
@@ -27,5 +32,16 @@ export class CitiesComponent {
 
        complete: () => { }
      });
+  }
+  ngOnInit() {
+    this.loadCities();
+  }
+
+  get postCity_NameControl(): any {
+    return this.postCityForm.controls['name'];
+  }
+
+  public postCitySubmitted() {
+
   }
 }
